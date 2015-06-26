96965b9c1087d7c48c2953f26886fd6a14eb3af1
William Rockwood <wrockwood@tadl.org>
2015-06-26 10:34:41 -0400

    conditional login/logged in in navbar
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  require 'open-uri'
+# Prevent CSRF attacks by raising an exception.
+# For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    require 'open-uri'
 
-  def shared_mock_variables
-    @user_name = 'William Rockwood'
-  end
+    def shared_mock_variables
+        @user_name = 'William Rockwood'
+    end
 
 end
