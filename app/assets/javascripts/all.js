51129cbf18b830b965497c32c73ccd00479c9c95
William Rockwood <wrockwood@tadl.org>
2015-06-26 21:54:06 -0400

    link to detail page from carousels, add root path route to point index at mock#index.
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -3,10 +3,20 @@ ready = function() {
     $(".tall-carousel").owlCarousel({
         items : 9,
         paginationNumbers : true,
+        itemsDesktop : [1199,7],
+        itemsDesktopSmall : [980,5],
+        itemsTablet: [768,4],
+        itemsMobile : [479,2],
+        itemsScaleUp : false,
     });
     $(".square-carousel").owlCarousel({
         items : 7,
         paginationNumbers : true,
+        itemsDesktop : [1199,5],
+        itemsDesktopSmall : [980,3],
+        itemsTablet: [768,3],
+        itemsTabletSmall: false,
+        itemsMobile : [479,1],
     });
 };
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'mock/details'
 
+  root :to => "mock#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
