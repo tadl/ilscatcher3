ebf738e8904dc1e7fdacb359f9728b6a4a9857ae
William Rockwood <wrockwood@tadl.org>
2015-07-29 15:34:01 -0400

    this isn't working, probably go back to jquery
    - since there's no *need* to do server-side rendered javascript
      for this and I was just doing it to be cute, I think I will back
      out of this plan and go back to jquery simplicity.
    - the dang preferences.js is loading, but does literally nothing.
      Oh well.
    
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/mock_controller.rb
+++ app/controllers/mock_controller.rb
@@ -259,6 +259,7 @@ class MockController < ApplicationController
       return
     end
     respond_to do |format|
+      format.js
       format.html
       format.json {render :json => {:user => @user, 
         :preferences => @preferences}}
