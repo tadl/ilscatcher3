a16190fb8abd8bb8c3742b796616d800f377187f
William Rockwood <wrockwood@tadl.org>
2016-01-27 11:39:52 -0500

    implement double-iframe in item details
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/util_controller.rb
+++ app/controllers/util_controller.rb
@@ -3,14 +3,15 @@ class UtilController < ApplicationController
     layout "youtube"
     respond_to :html
 
+    TRAILER_TEMPLATE = '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/%s?iv_load_policy=3&amp;modestbranding=1&amp;rel=0"></iframe></div>'.freeze
+
     def youtube
         id = params[:id]
         fetch_trailer = JSON.parse(open('https://trailer-tank.herokuapp.com/main/get_trailer.json?id=' + id, {:read_timeout => 1}).read) rescue nil
         if fetch_trailer.nil? || !fetch_trailer['message'] || fetch_trailer['message'] == 'error'
             trailer = ""
         else
-            trailer_id = fetch_trailer['message']
-            trailer = '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/' + trailer_id.to_s + '?iv_load_policy=3&amp;modestbranding=1&amp;rel=0"></iframe></div>'
+            trailer = TRAILER_TEMPLATE % fetch_trailer['message']
         end
         render :template => "util/youtube", :locals => {:trailer => trailer}
     end
