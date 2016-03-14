68f6f50572fff869b6b7484ab1e2025265e5c39a
William Rockwood <wrockwood@tadl.org>
2016-03-14 11:10:47 -0400

    added repo management scripts
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

---
+++ production-remotes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git remote add eastbay dokku@apps.tadl.org:eastbay-catalog
+git remote add fifelake dokku@apps.tadl.org:fifelake-catalog
+git remote add interlochen dokku@apps.tadl.org:interlochen-catalog
+git remote add interlochen-youth dokku@apps.tadl.org:interlochen-youth-catalog
+git remote add kalkaska dokku@apps.tadl.org:kalkaska-catalog
+git remote add kalkaska-teen dokku@apps.tadl.org:kalkaska-teen-catalog
+git remote add kalkaska-youth dokku@apps.tadl.org:kalkaska-youth-catalog
+git remote add kingsley dokku@apps.tadl.org:kingsley-catalog
+git remote add peninsula dokku@apps.tadl.org:peninsula-catalog
+git remote add tadl dokku@apps.tadl.org:catalog
+git remote add woodmere dokku@apps.tadl.org:woodmere-catalog
+git remote add woodmere-av dokku@apps.tadl.org:av-catalog
+git remote add woodmere-youth dokku@apps.tadl.org:youth-catalog
