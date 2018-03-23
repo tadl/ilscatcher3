64f3963e439a3187bc961559b02d12dbddad3e63
William Rockwood <wrockwood@tadl.org>
2018-03-23 14:43:23 -0400

    new screensaver image added, rework production-remotes script
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- production-remotes.sh
+++ production-remotes.sh
@@ -8,23 +8,26 @@ for x in $(git remote | grep -v origin); do
     git remote remove ${x}
 done
 
+# legacy opacs. remove when all opacs are replaced
 git remote add eastbay dokku@apps.tadl.org:eastbay-catalog
 git remote add fifelake dokku@apps.tadl.org:fifelake-catalog
-git remote add ica dokku@apps.tadl.org:ica
 git remote add interlochen dokku@apps.tadl.org:interlochen-catalog
 git remote add interlochen-youth dokku@apps.tadl.org:interlochen-youth-catalog
+git remote add kingsley dokku@apps.tadl.org:kingsley-catalog
+git remote add peninsula dokku@apps.tadl.org:peninsula-catalog
+git remote add woodmere dokku@apps.tadl.org:woodmere-catalog
+git remote add woodmere-youth dokku@apps.tadl.org:youth-catalog
+
+# contract libraries
 git remote add kalkaska dokku@apps.tadl.org:kalkaska-catalog
 git remote add kalkaska-teen dokku@apps.tadl.org:kalkaska-teen-catalog
 git remote add kalkaska-youth dokku@apps.tadl.org:kalkaska-youth-catalog
-git remote add kingsley dokku@apps.tadl.org:kingsley-catalog
-git remote add peninsula dokku@apps.tadl.org:peninsula-catalog
-git remote add tadl dokku@apps.tadl.org:catalog
-git remote add test dokku@apps.tadl.org:test-catalog
-git remote add woodmere dokku@apps.tadl.org:woodmere-catalog
-git remote add woodmere-av dokku@apps.tadl.org:av-catalog
-git remote add woodmere-youth dokku@apps.tadl.org:youth-catalog
+git remote add kcl dokku@appstwo.tadl.org:kalkaska-catalog
 git remote add sbbdl dokku@appstwo.tadl.org:sbbdl
+git remote add ica dokku@appstwo.tadl.org:ica
 
-git remote add tadl-catalog dokku@appstwo.tadl.org:tadl-catalog
-git remote add kalkaska-catalog dokku@appstwo.tadl.org:kalkaska-catalog
-git remote add ica-myaccount dokku@appstwo.tadl.org:ica
+# tadl
+git remote add tadl dokku@appstwo.tadl.org:tadl-catalog
+
+# dev
+git remote add test dokku@apps.tadl.org:test-catalog
