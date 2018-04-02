8893d7006302dd075707ef1ed351c8a885a20ad8
William Rockwood <wrockwood@tadl.org>
2018-04-02 09:54:49 -0400

    updated production remotes script
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- production-remotes.sh
+++ production-remotes.sh
@@ -8,28 +8,15 @@ for x in $(git remote | grep -v origin); do
     git remote remove ${x}
 done
 
-# legacy opacs. remove when all opacs are replaced
-git remote add eastbay dokku@apps.tadl.org:eastbay-catalog
-git remote add fifelake dokku@apps.tadl.org:fifelake-catalog
-git remote add interlochen dokku@apps.tadl.org:interlochen-catalog
-git remote add interlochen-youth dokku@apps.tadl.org:interlochen-youth-catalog
-git remote add kingsley dokku@apps.tadl.org:kingsley-catalog
-git remote add peninsula dokku@apps.tadl.org:peninsula-catalog
-git remote add woodmere dokku@apps.tadl.org:woodmere-catalog
-git remote add woodmere-youth dokku@apps.tadl.org:youth-catalog
+# tadl
+git remote add tadl dokku@appstwo.tadl.org:tadl-catalog
 
 # contract libraries
-git remote add kalkaska-teen dokku@apps.tadl.org:kalkaska-teen-catalog #legacy
-git remote add kalkaska-youth dokku@apps.tadl.org:kalkaska-youth-catalog #legacy
-
 git remote add kcl dokku@appstwo.tadl.org:kalkaska-catalog
 git remote add kcl-youth dokku@appstwo.tadl.org:kalkaska-youth
 git remote add kcl-teen dokku@appstwo.tadl.org:kalkaska-teen
 git remote add sbbdl dokku@appstwo.tadl.org:sbbdl
 git remote add ica dokku@appstwo.tadl.org:ica
-
-# tadl
-git remote add tadl dokku@appstwo.tadl.org:tadl-catalog
 
 # dev
 git remote add test dokku@apps.tadl.org:test-catalog
