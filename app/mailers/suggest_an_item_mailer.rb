2a35ba6efa719994cd7f067231e2cf3c51c9c1b7
William Rockwood <wrockwood@tadl.org>
2018-04-26 10:58:59 -0400

    configure a system name for use in places
    - immediately useful for the suggest an item mailer template
    
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/mailers/suggest_an_item_mailer.rb
+++ app/mailers/suggest_an_item_mailer.rb
@@ -2,8 +2,8 @@ class SuggestAnItemMailer < ActionMailer::Base
   default from: "tadlbot@tadl.org"
 
   def suggest_an_item_email(info)
-  	@users = ENV["SUGGEST_AN_ITEM_EMAIL"]
-  	@info = info
-  	mail to: @users, subject: 'TADL - Suggest An Item'
+    @users = ENV["SUGGEST_AN_ITEM_EMAIL"]
+    @info = info
+    mail to: @users, subject: '<%= Settings.system_name %> - Suggest An Item'
   end
 end
